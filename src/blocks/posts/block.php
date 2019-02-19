2b64c61a66ef0a9568a8d520303550b0163c91b9
hemratna <hemratna.bhimani@gmail.com>
2019-02-19 14:56:52 +0530

    PHP code cleanup

--- src/blocks/posts/block.php
+++ src/blocks/posts/block.php
@@ -1,7 +1,6 @@
 <?php
-// block.php
 
-function my_plugin_render_block_latest_post( $attributes, $content ) {
+function sevenspan_block_posts( $attributes, $content ) {
     $posts = wp_get_recent_posts( array(
         'numberposts' => $attributes['noOfPosts'],
         'post_status' => 'publish',
@@ -9,24 +8,22 @@ function my_plugin_render_block_latest_post( $attributes, $content ) {
     if ( count( $posts ) === 0 ) {
         return 'No posts';
     }
-
     $postMarkup = '';
-
     foreach($posts as $post){
-      $post_id = $post['ID'];
-      $postMarkup .= '<h3>';
-      $postMarkup .=get_the_title( $post_id );
-      $postMarkup .= get_the_post_thumbnail( $post_id , 'blog-thumb' );
-      $postMarkup .='</h3>';
+        $post_id = $post['ID'];
+        $postMarkup .= '<h3>';
+        $postMarkup .= get_the_title( $post_id );
+        $postMarkup .= get_the_post_thumbnail( $post_id , 'blog-thumb' );
+        $postMarkup .='</h3>';
     }
     return $postMarkup;
 }
 
 register_block_type( 'sevenspan/posts', array(
     'attributes' => array(
-      'noOfPosts' => array(
-          'type' => 'number',
-      )
+        'noOfPosts' => array(
+            'type' => 'number',
+        )
     ),
-    'render_callback' => 'my_plugin_render_block_latest_post',
+    'render_callback' => 'sevenspan_block_posts',
 ) );
